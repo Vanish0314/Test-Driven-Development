a056ffc7e257724de5eba73cb36567a3c44607c7
vanishxiao <vanishxiao@tencent.com>
2025-07-16 20:19:54 +0800

    project frame

---
+++ LogAndNotification.Tests/Src/LogAnalyzerTests.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class LogAnalyzerTests
+{
+    [Test]
+    public void IsValidFileName_BadExtension_ReturnsFalse()
+    {
+        // 测试代码写在这里
+    }
+}
---
+++ LogAndNotification/Src/LogAnalyzer.cs
@@ -0,0 +1,11 @@
+public class LogAnalyzer
+{
+    public bool IsValidLogFileName(string fileName)
+    {
+        if (fileName.EndsWith(".SLF"))
+        {
+            return false;
+        }
+        return true;
+    }
+}
